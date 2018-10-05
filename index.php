bf7826222cb882d7d76b6d9f3de2a7c2b3958484
tiffany law <ykltiffany@gmail.com>
2018-10-05 15:51:58 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Welcome World";
+
+
+echo "<br><br>Finally DONE !!!!!";
+
+echo "<br><br>GIT PULL repo zepto";
+
+echo "<br><br>GIT PUSH to master... only allow tiffany1845";
+echo "<br><br>GIT PUSH to master... only allow tiffany1845 2";
+echo "<br><br>GIT PUSH to master... only allow tiffany1845 3";
+
+?>
