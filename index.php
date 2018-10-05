6730c44d9f0cad4c8bb0c4a7e8720ee427781b88
tiffany law <ykltiffany@gmail.com>
2018-10-05 16:17:08 +0800

    control push master

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ echo "Welcome World";
 echo "<br><br>Finally DONE !!!!!";
 
 echo "<br><br>GIT PUSH repo zepto2 auto deploy to live server";
+echo "<br><br>GIT PUSH repo zepto2 control tiffany1845 can push master 1";
 
 ?>
